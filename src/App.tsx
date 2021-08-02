a0b247ceb8aa8a467239a892e36fc7d586a70926
Arthur Cabral <arthurcabral.dev@gmail.com>
2021-08-02 09:34:10 -0300

    📃 - Initial Application

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import 'react-calendar-heatmap/dist/styles.css';
+
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Profile from './pages/Profile';
+import Repo from './pages/Repo';
+
+import { ThemeName, themes } from './styles/themes';
+
+function App() {
+  const [ themeName, setThemeName ] = useState<ThemeName>('light');
+  const currentTheme = themes[themeName];
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <BrowserRouter>
+        <Header themeName={themeName} setThemeName={setThemeName} />
+
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/:username/:reponame" element={<Repo />} />
+        </Routes>
+
+        <Footer />
+
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
