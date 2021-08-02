a48647f99e8741f891e65d73595a7fe0d9b38e08
Arthur Cabral <arthurcabral.dev@gmail.com>
2021-08-02 10:24:04 -0300

    🧩 - Footer Component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Container, Line, GithubLog } from './styles';
+
+const Footer: React.FC = () => {
+  return (
+    <Container>
+      <Line />
+      <GithubLog />
+    </Container>
+  );
+}
+
+export default Footer;
